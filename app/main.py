d8495691c59913d4f135c584ced790ec0964fe1b
Ilari Pihlajisto <ilaripih@gmail.com>
2019-11-11 23:19:27 +0200

    increase grpc max response size

--- app/main.py
+++ app/main.py
@@ -21,16 +21,25 @@ def predict(stub, np_images):
 
 
 def main(_):
-    channel = grpc.insecure_channel('local_serving:8500')
+    # The "raw_detection_boxes" and "raw_detection_scores" tensors
+    # bloat the response size.
+    channel = grpc.insecure_channel('local_serving:8500', options=[
+        ('grpc.max_receive_message_length', 100 * 1024 * 1024)
+    ])
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
     batch_size = 4
-    n_requests = 250
+    n_requests = 25
     start_time = time.time()
     for _ in range(n_requests):
         predict(stub, np.zeros(shape=(batch_size, 640, 640, 3), dtype=np.uint8))
     elapsed_time = time.time() - start_time
     n_images = batch_size * n_requests
+
+    # Results with GTX 1060 (6GB):
+    # Serving 1.15.0-gpu: 0.22 FPS
+    # Serving 1.14.0-gpu: 4.19 FPS
+    # Serving 2.0.0-gpu: 4.05 FPS
     print('{} images in {:.2f} ({:.2f} FPS)'.format(
         n_images, elapsed_time, n_images / elapsed_time))
 
